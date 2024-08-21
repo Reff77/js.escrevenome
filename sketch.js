8d6f11a47f5714e44d8606108fa21675f6232365
Reff77 <dasilvajenniferferreira@gmail.com>
2024-08-21 13:37:59 -0300

    Create sketch.js
    adicionei arquivo js

---
+++ sketch.js
@@ -0,0 +1,11 @@
+let cor;
+let circuloX; // horizontal
+let circuloY; // vertical
+
+function setup() {
+  createCanvas(400, 400); // width x height
+  background(color(100,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  circuloX = [0,0,0];
+  circuloY = [random(height), random(height), random(height)];
+}
